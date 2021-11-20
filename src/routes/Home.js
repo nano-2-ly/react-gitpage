2cfafe6a5c94ec9bbc4f051cd096a0d8a0f74872
nanotoly <43829220+nano-2-ly@users.noreply.github.com>
2021-11-20 22:35:42 +0900

    first commit

---
+++ src/routes/Home.js
@@ -0,0 +1,40 @@
+import { useState, useEffect} from "react";
+import Movie from "../components/Movie";
+
+function Home(){
+    
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async() =>{
+        const json = await (await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+        )).json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    }
+
+
+    useEffect(()=>{
+        getMovies(); 
+
+    },[])
+    console.log(movies);
+    return(
+        <div>
+        {loading?<h1>loading</h1>: (<div>
+            {movies.map(item=>
+            <Movie 
+                key={item.id} 
+                id={item.id}
+                coverImg={item.medium_cover_image} 
+                title={item.title} 
+                summary={item.summary} 
+                genres={item.genres} 
+            />
+            )}
+            </div>)}
+        </div>
+    );
+}
+
+export default Home;
